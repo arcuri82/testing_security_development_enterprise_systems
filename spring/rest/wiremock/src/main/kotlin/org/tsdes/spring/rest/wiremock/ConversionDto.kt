c7a88b24784f51235ab5d79601cb9500ab8db333
arcuri82 <arcuri82@gmail.com>
2017-08-03 16:58:30 +0200

    wiremock example

---
+++ spring/rest/wiremock/src/main/kotlin/org/tsdes/spring/rest/wiremock/ConversionDto.kt
@@ -0,0 +1,20 @@
+package org.tsdes.spring.rest.wiremock
+
+import javax.xml.bind.annotation.XmlElement
+import javax.xml.bind.annotation.XmlRootElement
+
+/**
+ * Created by arcuri82 on 02-Aug-17.
+ */
+@XmlRootElement
+data class ConversionDto(
+
+        @XmlElement(required = true)
+        var from: String? = null,
+
+        @XmlElement(required = true)
+        var to: String? = null,
+
+        @XmlElement(required = true)
+        var rate: Double? = null
+)

853317d9028a41c670be4fc9fcab3b4e28384199
arcuri82 <arcuri82@gmail.com>
2017-08-11 16:54:09 +0200

    fixed timing issue in Docker test

--- spring/amqp/amqp-rest/e2e-tests/src/test/kotlin/org/tsdes/spring/amqp/rest/e2etests/IntegrationDockerIT.kt
+++ spring/amqp/amqp-rest/e2e-tests/src/test/kotlin/org/tsdes/spring/amqp/rest/e2etests/IntegrationDockerIT.kt
@@ -13,16 +13,52 @@ import java.io.File
  */
 class IntegrationDockerIT {
 
-    companion object {
+//    companion object {
+//
+//        class KDockerComposeContainer(path: File) : DockerComposeContainer<KDockerComposeContainer>(path)
+//
+//        @ClassRule @JvmField
+//        val env = KDockerComposeContainer(File("../docker-compose.yml"))
+//                .withLocalCompose(true)
+//
+//    }
 
-        class KDockerComposeContainer(path: File) : DockerComposeContainer<KDockerComposeContainer>(path)
-
-        @ClassRule @JvmField
-        val env = KDockerComposeContainer(File("../docker-compose.yml"))
+    private fun waitForSpring(port: Int, timeoutMS: Long) {
+        assertWithinTime(timeoutMS, {
+            given().port(port).get("/health").then().body("status", equalTo("UP"))
+        })
     }
 
-    //TODO wait for init
+    private fun assertWithinTime(timeoutMS: Long, lambda: () -> Any) {
+        val start = System.currentTimeMillis()
 
+        var delta = 0L
+
+        while (delta < timeoutMS) {
+            try {
+                lambda.invoke()
+                return
+            } catch (e: AssertionError) {
+                Thread.sleep(100)
+                delta = System.currentTimeMillis() - start
+            }
+        }
+
+        lambda.invoke()
+    }
+
+    /*
+        TODO
+
+        This will be fixed in next release of TestContainers, see
+
+        https://github.com/testcontainers/testcontainers-java/issues/439#issuecomment-321812314
+
+        For now, can be run manually with by first running
+
+        docker-compose build
+        docker-compose up
+     */
     @Ignore
     @Test
     fun test() {
@@ -31,28 +67,35 @@ class IntegrationDockerIT {
         val receiver0 = 9001
         val receiver1 = 9002
 
-        given().port(receiver0)
+        waitForSpring(senderPort, 30_000)
+        waitForSpring(receiver0, 2_000)
+        waitForSpring(receiver1, 2_000)
+
+        val n = given().port(receiver0)
                 .get("counter")
                 .then()
                 .statusCode(200)
-                .body(equalTo("0"))
+                .extract().body().`as`(Int::class.java)
 
 
         given().port(senderPort).body("foo_0").post("/sender").then().statusCode(204)
 
-        given().port(receiver0)
-                .get("counter")
-                .then()
-                .statusCode(200)
-                .body(equalTo("1"))
+        assertWithinTime(3000, {
+            given().port(receiver0)
+                    .get("counter")
+                    .then()
+                    .statusCode(200)
+                    .body(equalTo("${n+1}"))
+        })
 
         given().port(senderPort).body("foo_1").post("/sender").then().statusCode(204)
 
-        given().port(receiver1)
-                .get("counter")
-                .then()
-                .statusCode(200)
-                .body(equalTo("2"))
+        assertWithinTime(3000, {
+            given().port(receiver1)
+                    .get("counter")
+                    .then()
+                    .statusCode(200)
+                    .body(equalTo("${n+2}"))
+        })
     }
-
 }

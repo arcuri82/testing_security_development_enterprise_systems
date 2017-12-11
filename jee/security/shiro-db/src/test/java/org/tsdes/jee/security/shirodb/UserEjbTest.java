db363f185773a9c36cfb6d4a6acf2ac7a0a78e77
arcuri82 <arcuri82@gmail.com>
2017-12-11 12:43:57 +0100

    fix for Arquillian pom bug

--- jee/security/shiro-db/src/test/java/org/tsdes/jee/security/shirodb/UserEjbTest.java
+++ jee/security/shiro-db/src/test/java/org/tsdes/jee/security/shirodb/UserEjbTest.java
@@ -1,5 +1,9 @@
 package org.tsdes.jee.security.shirodb;
 
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
@@ -14,8 +18,14 @@ public class UserEjbTest {
     public static JavaArchive createDeployment() {
 
         return ShrinkWrap.create(JavaArchive.class)
-                .addClasses(A.class, Data.class)
+                .addPackages(true, "org.tsdes")
+                .addPackages(true, "org.apache.shiro")
+                .addAsResource("createDB.sql")
                 .addAsResource("META-INF/persistence.xml");
     }
 
+    @Test
+    public void testCreateUser(){
+
+    }
 }

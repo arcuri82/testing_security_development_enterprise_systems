5eef712b18eb6cc30e50a1da0eda953aec564b5b
arcuri82 <arcuri82@gmail.com>
2017-12-11 12:25:03 +0100

    fixing dependency scope for Arquillian

--- jee/security/shiro-db/src/test/java/org/tsdes/jee/security/shirodb/UserEjbTest.java
+++ jee/security/shiro-db/src/test/java/org/tsdes/jee/security/shirodb/UserEjbTest.java
@@ -1,10 +1,21 @@
 package org.tsdes.jee.security.shirodb;
 
+import org.junit.runner.RunWith;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by arcuri82 on 11-Dec-17.
  */
+@RunWith(Arquillian.class)
 public class UserEjbTest {
+
+    @Deployment
+    public static JavaArchive createDeployment() {
+
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClasses(A.class, Data.class)
+                .addAsResource("META-INF/persistence.xml");
+    }
 
 }

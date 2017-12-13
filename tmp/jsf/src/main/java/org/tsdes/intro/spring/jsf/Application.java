610fcde9b5f9208b4c4a80e0b188ca1e4aa3a140
arcuri82 <arcuri82@gmail.com>
2017-12-13 11:58:39 +0100

    jsf in spring

---
+++ tmp/jsf/src/main/java/org/tsdes/intro/spring/jsf/Application.java
@@ -0,0 +1,17 @@
+package org.tsdes.intro.spring.jsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by arcuri82 on 12-Dec-17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+}

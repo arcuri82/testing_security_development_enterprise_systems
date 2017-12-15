e6cace309d13a9d3a96cf06f7ee9e64f55884e22
arcuri82 <arcuri82@gmail.com>
2017-12-15 16:08:29 +0100

    fixed DB configuration

--- intro/exercises/quiz-game/part-11/frontend/src/test/java/org/tsdes/intro/exercises/quizgame/LocalApplicationRunner.java
+++ intro/exercises/quiz-game/part-11/frontend/src/test/java/org/tsdes/intro/exercises/quizgame/LocalApplicationRunner.java
@@ -8,6 +8,6 @@ import org.springframework.boot.SpringApplication;
 public class LocalApplicationRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, "--spring.profiles.active=test");
     }
 }

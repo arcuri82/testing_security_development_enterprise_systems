e6da1822e6544dfc82098b015a0bd2bd81c4ead3
arcuri82 <arcuri82@gmail.com>
2017-12-23 13:49:55 +0100

    test for quiz part-01

---
+++ intro/exercises/quiz-game/part-01/src/test/java/org/tsdes/intro/exercises/quizgame/entity/QuizEntityTest.java
@@ -0,0 +1,35 @@
+package org.tsdes.intro.exercises.quizgame.entity;
+
+
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class QuizEntityTest {
+
+    @Test
+    public void testQuiz(){
+
+        Quiz quiz = new Quiz();
+        quiz.setQuestion("Will this test pass?");
+        quiz.setFirstAnswer("Yes");
+        quiz.setSecondAnswer("No");
+        quiz.setThirdAnswer("Maybe");
+        quiz.setFourthAnswer("No idea");
+        quiz.setIndexOfCorrectAnswer(0);
+
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("DB");
+        EntityManager em = factory.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        em.persist(quiz);
+
+        tx.commit();
+    }
+
+}

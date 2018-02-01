d1dcd0747af8306fb103541e2a2bbfb8e33fb446
arcuri82 <arcuri82@gmail.com>
2018-02-01 13:56:30 +0100

    spring jsf

--- intro/spring/server/jsf/src/main/java/org/tsdes/intro/spring/jsf/Application.java
+++ intro/spring/server/jsf/src/main/java/org/tsdes/intro/spring/jsf/Application.java
@@ -9,6 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    /**
+     * In a "packaged" Java application, this method would be the entry point.
+     * This is also useful to have for debugging, as in
+     * an IDE you can just right-click on this class to start
+     * the whole application.
+     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }

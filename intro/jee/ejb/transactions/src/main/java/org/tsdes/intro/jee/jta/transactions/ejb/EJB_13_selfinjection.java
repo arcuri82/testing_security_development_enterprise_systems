6f03aa0732df9f2acb623f853737b5a2501f605c
arcuri82 <arcuri82@gmail.com>
2018-02-13 15:03:58 +0100

    added example on self-injection

---
+++ intro/jee/ejb/transactions/src/main/java/org/tsdes/intro/jee/jta/transactions/ejb/EJB_13_selfinjection.java
@@ -0,0 +1,51 @@
+package org.tsdes.intro.jee.jta.transactions.ejb;
+
+import org.tsdes.intro.jee.jta.transactions.data.Foo;
+
+import javax.ejb.*;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static javax.ejb.TransactionAttributeType.NOT_SUPPORTED;
+
+/**
+ * Created by arcuri82 on 13-Feb-18.
+ */
+@Singleton
+public class EJB_13_selfinjection {
+
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @EJB
+    private EJB_13_selfinjection proxy;
+
+    public boolean createFailDirect() {
+
+        try {
+            String name = "bar";
+            Foo foo = new Foo(name);
+            Foo copy = new Foo(name);
+
+            em.persist(foo);
+            em.persist(copy);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @TransactionAttribute(NOT_SUPPORTED)
+    public boolean createFailIndirect() {
+
+        try {
+            proxy.createFailDirect();
+        } catch (EJBException e) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ intro/jee/ejb/transactions/src/test/java/org/tsdes/intro/jee/jta/transactions/ejb/EJB_13_selfinjectionTest.java
@@ -0,0 +1,38 @@
+package org.tsdes.intro.jee.jta.transactions.ejb;
+
+import org.junit.Test;
+
+import javax.ejb.EJBException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by arcuri82 on 13-Feb-18.
+ */
+public class EJB_13_selfinjectionTest extends TestBase{
+
+
+    @Test
+    public void testDirectCall(){
+
+        EJB_13_selfinjection ejb = getEJB(EJB_13_selfinjection.class);
+
+        try{
+            //this does not return a boolean, but rather throw an exception
+            ejb.createFailDirect();
+            fail();
+        }catch (EJBException e){
+            //expected
+        }
+    }
+
+    @Test
+    public void testIndirect(){
+
+        EJB_13_selfinjection ejb = getEJB(EJB_13_selfinjection.class);
+
+        boolean result = ejb.createFailIndirect();
+
+        assertFalse(result);
+    }
+}

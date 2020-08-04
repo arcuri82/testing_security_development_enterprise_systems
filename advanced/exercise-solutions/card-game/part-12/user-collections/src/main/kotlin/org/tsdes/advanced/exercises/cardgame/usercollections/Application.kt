5522f8ee5168b0a172cac53936471bf0b51660b2
arcuri82 <arcuri82@gmail.com>
2020-08-04 14:12:56 +0200

    starting with exercises

---
+++ advanced/exercise-solutions/card-game/part-12/user-collections/src/main/kotlin/org/tsdes/advanced/exercises/cardgame/usercollections/Application.kt
@@ -0,0 +1,7 @@
+package org.tsdes.advanced.exercises.cardgame.usercollections
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application {
+}

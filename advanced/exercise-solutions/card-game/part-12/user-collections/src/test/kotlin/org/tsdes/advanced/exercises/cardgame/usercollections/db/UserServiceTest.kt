ffadbf4766bfd0653803c5a30ffd47401a652ca6
arcuri82 <arcuri82@gmail.com>
2020-08-05 13:44:47 +0200

    tests and Flyway

--- advanced/exercise-solutions/card-game/part-12/user-collections/src/test/kotlin/org/tsdes/advanced/exercises/cardgame/usercollections/db/UserServiceTest.kt
+++ advanced/exercise-solutions/card-game/part-12/user-collections/src/test/kotlin/org/tsdes/advanced/exercises/cardgame/usercollections/db/UserServiceTest.kt
@@ -47,12 +47,103 @@ internal class UserServiceTest{
 
     @Test
     fun testCreateUser(){
-
         val id = "foo"
-
-        userService.registerNewUser(id)
-
+        assertTrue(userService.registerNewUser(id))
         assertTrue(userRepository.existsById(id))
     }
 
+    @Test
+    fun testFailCreateUserTwice(){
+        val id = "foo"
+        assertTrue(userService.registerNewUser(id))
+        assertFalse(userService.registerNewUser(id))
+    }
+
+    @Test
+    fun testBuyCard(){
+
+        val userId = "foo"
+        val cardId = "c00"
+
+        userService.registerNewUser(userId)
+        userService.buyCard(userId, cardId)
+
+        val user = userService.findByIdEager(userId)!!
+        assertTrue(user.ownedCards.any { it.cardId == cardId})
+    }
+
+    @Test
+    fun testBuyCardFailNotEnoughMoney(){
+
+        val userId = "foo"
+        val cardId = "c09"
+        userService.registerNewUser(userId)
+
+        val e = assertThrows(IllegalArgumentException::class.java){
+            userService.buyCard(userId, cardId)
+        }
+        assertTrue(e.message!!.contains("coin"), "Wrong error message: ${e.message}")
+    }
+
+
+    @Test
+    fun testOpenPack(){
+
+        val userId = "foo"
+        userService.registerNewUser(userId)
+
+        val before = userService.findByIdEager(userId)!!
+        val totCards = before.ownedCards.sumBy { it.numberOfCopies }
+        val totPacks = before.cardPacks
+        assertTrue(totPacks > 0)
+
+        userService.openPack(userId)
+
+        val after = userService.findByIdEager(userId)!!
+        assertEquals(totPacks - 1, after.cardPacks)
+        assertEquals(totCards + UserService.CARDS_PER_PACK,
+                after.ownedCards.sumBy { it.numberOfCopies }  )
+    }
+
+    @Test
+    fun testOpenPackFail(){
+
+        val userId = "foo"
+        userService.registerNewUser(userId)
+
+        val before = userService.findByIdEager(userId)!!
+        val totPacks = before.cardPacks
+
+        repeat(totPacks){
+            userService.openPack(userId)
+        }
+
+        val after = userService.findByIdEager(userId)!!
+        assertEquals(0, after.cardPacks)
+
+        assertThrows(IllegalArgumentException::class.java){
+            userService.openPack(userId)
+        }
+    }
+
+    @Test
+    fun testMillCard(){
+
+        val userId = "foo"
+        userService.registerNewUser(userId)
+
+        val before = userRepository.findById(userId).get()
+        val coins = before.coins
+
+        userService.openPack(userId)
+
+        val between = userService.findByIdEager(userId)!!
+        val n = between.ownedCards.sumBy { it.numberOfCopies }
+        userService.millCard(userId, between.ownedCards[0].cardId!!)
+
+
+        val after = userService.findByIdEager(userId)!!
+        assertTrue(after.coins > coins)
+        assertEquals(n-1, after.ownedCards.sumBy { it.numberOfCopies })
+    }
 }

0e08eda0572b410b2bddbbd9236f20895e90501c
arcuri82 <arcuri82@gmail.com>
2020-08-07 23:53:41 +0200

    Replaced Hystrix with Spring Circuit Breaker

--- advanced/rest/circuit-breaker/src/main/kotlin/org/tsdes/advanced/rest/circuitbreaker/YRest.kt
+++ advanced/rest/circuit-breaker/src/main/kotlin/org/tsdes/advanced/rest/circuitbreaker/YRest.kt
@@ -1,18 +1,22 @@
 package org.tsdes.advanced.rest.circuitbreaker
 
-import com.netflix.hystrix.HystrixCommand
-import com.netflix.hystrix.HystrixCommandGroupKey
+
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
 import java.net.URI
+import javax.annotation.PostConstruct
 
 /**
  * Created by arcuri82 on 03-Aug-17.
  */
 @RestController
-class YRest {
+class YRest(
+        private val circuitBreakerFactory : Resilience4JCircuitBreakerFactory
+) {
 
     /*
         Note: here for simplicity I am calling another service in the
@@ -25,6 +29,13 @@ class YRest {
 
     private val client: RestTemplate = RestTemplate()
 
+    private  var cb : CircuitBreaker? =null
+
+    @PostConstruct
+    fun init(){
+        //FIXME: unfortunately this does not work, as factory not initialized yet :(
+        //cb = circuitBreakerFactory.create("circuitBreakerToX")
+    }
 
     @GetMapping(path = ["/y"])
     fun doGetSingle(
@@ -32,44 +43,23 @@ class YRest {
             v: Long
     ): Long {
 
+        if (cb == null)
+            cb = circuitBreakerFactory.create("circuitBreakerToX")
+
         /*
             this is synchronous, but would still give an answer within the given time (or
             immediately if the circuit breaker is on)
          */
-        return CallX(v).execute()
-    }
 
-
-    /*
-        TODO: note that Hystrix is now no longer in active development, although it is maintained.
-        https://github.com/Netflix/Hystrix
-        Once Spring Cloud Circuit Breaker is out of incubation phase, it should be replaced here
-        https://spring.io/blog/2019/04/16/introducing-spring-cloud-circuit-breaker
-     */
-
-    /*
-        Calls to an external web service will be wrapped into a HystrixCommand
-     */
-    private inner class CallX(private val x: Long)
-        : HystrixCommand<Long>(HystrixCommandGroupKey.Factory.asKey("Interactions with X")) {
-
-        override fun run(): Long {
-
-            /*
-                Note: this synchronous call could fail (and so throw an exception),
-                or even just taking a long while (if server is under heavy load)
-             */
-
-            val uri = URI("http://localhost:$port/x")
-
-            val result = client.postForEntity(uri, x, Long::class.java)
-
-            return result.body
-        }
-
-        override fun getFallback(): Long {
-            //this is what is returned in case of exceptions or timeouts
-            return 0L
-        }
+        return cb!!.run(
+                //the command, doing the HTTP call
+                {
+                    val uri = URI("http://localhost:$port/x")
+                    val result = client.postForEntity(uri, v, Long::class.java)
+                    result.body
+                },
+                //this is what is returned in case of exceptions or timeouts
+                { e -> 0L }
+        )
     }
 }

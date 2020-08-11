fe5241673bef0b5047e16ace7830590e840c8cfa
arcuri82 <arcuri82@gmail.com>
2020-08-11 17:14:56 +0200

    Adding Gateway and Discovery

--- advanced/exercise-solutions/card-game/part-12/cards/src/main/kotlin/org/tsdes/advanced/exercises/cardgame/cards/Application.kt
+++ advanced/exercise-solutions/card-game/part-12/cards/src/main/kotlin/org/tsdes/advanced/exercises/cardgame/cards/Application.kt
@@ -2,6 +2,7 @@ package org.tsdes.advanced.exercises.cardgame.cards
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.context.annotation.Bean
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
@@ -10,6 +11,7 @@ import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 
 @SpringBootApplication(scanBasePackages = ["org.tsdes.advanced"])
+@EnableDiscoveryClient
 class Application {
 
     @Bean

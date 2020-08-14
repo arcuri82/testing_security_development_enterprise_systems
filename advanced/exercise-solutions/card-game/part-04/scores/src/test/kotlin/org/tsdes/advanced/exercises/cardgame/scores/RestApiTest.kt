09060e5c90ff8c0d89396edc2e28d2167e6353dc
arcuri82 <arcuri82@gmail.com>
2020-08-14 15:25:43 +0200

    exercise part-04

---
+++ advanced/exercise-solutions/card-game/part-04/scores/src/test/kotlin/org/tsdes/advanced/exercises/cardgame/scores/RestApiTest.kt
@@ -0,0 +1,95 @@
+package org.tsdes.advanced.exercises.cardgame.scores
+
+import io.restassured.RestAssured
+import io.restassured.common.mapper.TypeRef
+import io.restassured.http.ContentType
+import org.hamcrest.CoreMatchers
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.tsdes.advanced.exercises.cardgame.scores.db.UserStatsRepository
+import org.tsdes.advanced.rest.dto.PageDto
+import javax.annotation.PostConstruct
+
+
+@ActiveProfiles("FakeData,test")
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(classes = [(Application::class)],
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+internal class RestApiTest{
+
+    @LocalServerPort
+    protected var port = 0
+
+    @Autowired
+    private lateinit var repository: UserStatsRepository
+
+    @PostConstruct
+    fun init(){
+        RestAssured.baseURI = "http://localhost"
+        RestAssured.port = port
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
+    }
+
+    val page : Int = 10
+
+    @Test
+    fun testGetPage() {
+
+        RestAssured.given().accept(ContentType.JSON)
+                .get("/api/scores")
+                .then()
+                .statusCode(200)
+                .body("data.list.size()", CoreMatchers.equalTo(page))
+    }
+
+
+    @Test
+    fun testAllPages(){
+
+        val read = mutableSetOf<String>()
+
+        var page = RestAssured.given().accept(ContentType.JSON)
+                .get("/api/scores")
+                .then()
+                .statusCode(200)
+                .body("data.list.size()", CoreMatchers.equalTo(page))
+                .extract().body().jsonPath().getObject("data",object: TypeRef<PageDto<Map<String, Object>>>(){})
+        read.addAll(page.list.map { it["userId"].toString()})
+
+        checkOrder(page)
+
+        while(page.next != null){
+            page = RestAssured.given().accept(ContentType.JSON)
+                    .get(page.next)
+                    .then()
+                    .statusCode(200)
+                    .extract().body().jsonPath().getObject("data",object: TypeRef<PageDto<Map<String, Object>>>(){})
+            read.addAll(page.list.map { it["userId"].toString()})
+            checkOrder(page)
+        }
+
+        val total = repository.count().toInt()
+
+        //recall that sets have unique elements
+        assertEquals(total, read.size)
+    }
+
+    private fun checkOrder(page: PageDto<Map<String, Object>>) {
+        for (i in 0 until page.list.size - 1) {
+            val ascore = page.list[i]["score"].toString().toInt()
+            val bscore = page.list[i + 1]["score"].toString().toInt()
+            val aid = page.list[i]["userId"].toString()
+            val bid = page.list[i + 1]["userId"].toString()
+            assertTrue(ascore >= bscore)
+            if (ascore == bscore) {
+                assertTrue(aid > bid)
+            }
+        }
+    }
+}
--- doc/advanced/exercises/card-game/card-game-part-04.md
+++ doc/advanced/exercises/card-game/card-game-part-04.md
@@ -1,6 +1,9 @@
+# Card Game - Part 04
 
 In the REST API of `user-collections`, update each endpoint
 to use wrapped responses, and customized error handling (as seen in class).
+Every time there is a user error, return a meaningful error message in the 
+HTTP response. 
 
 To use wrapped responses and custom error handling based on them, 
 you need to rely on the classes from the modules:
@@ -26,3 +29,45 @@ spring:
   resources:
     add-mappings: false
 ```
+
+Create a new module `scores` for a REST API that keeps track of the scores
+of all players (based on victories and defeats in the game), and provides
+a leaderboard.
+
+This new API should have the same structures and features of `user-collections`, e.g.:
+
+* A self-executable uber-jar file.
+* Postgres database with Flyway, and embedded H2 for the tests.
+* Wrapped responses with custom error handling.
+* Documentation with SpringFox.
+* Distinction between DTOs and JPA entities. 
+* Test cases, both with RestAssured and directly on the `@Service` classes,
+  achieving at least 70% code coverage in total.
+  
+In the database, for each user you need to save data for:
+userId, victories, defeats, draws and score. 
+
+The REST API needs the following endpoints:
+
+* `PUT /api/scores/{userId}`, create default stats info (no victories, no defeats) for the given user.  
+* `GET /api/scores/{userId}`, get the stats info for the given user.  
+* `GET /api/scores`, get the stats of all players. This must be handled with keyset-pagination,
+    sorted based on the `score` field (top to bottom).
+    
+Inside `src/main/kotlin`, create a `FakeDataService` that generates 
+*n* player info at random (e.g., *n=50*).
+This bean should be activated based only with a profile (using `@Profile`).
+
+
+Solutions to this exercise can be found in the 
+`advanced/exercise-solutions/card-game/part-04` module. 
+
+      
+
+  
+
+
+
+
+
+

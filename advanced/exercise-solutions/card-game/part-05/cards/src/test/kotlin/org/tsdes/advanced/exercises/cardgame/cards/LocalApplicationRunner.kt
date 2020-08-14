6b844ce243df67978bf23698582de42a3b1661a5
arcuri82 <arcuri82@gmail.com>
2020-08-14 16:07:10 +0200

    exercise part-05

---
+++ advanced/exercise-solutions/card-game/part-05/cards/src/test/kotlin/org/tsdes/advanced/exercises/cardgame/cards/LocalApplicationRunner.kt
@@ -0,0 +1,8 @@
+package org.tsdes.advanced.exercises.cardgame.cards
+
+import org.springframework.boot.SpringApplication
+
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, "--spring.profiles.active=test")
+}

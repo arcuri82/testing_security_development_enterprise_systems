e118888b2f0db07d187d91a887ebe30dedd6a62f
arcuri82 <arcuri82@gmail.com>
2020-08-16 21:39:11 +0200

    starting diplaying cards

--- advanced/exercise-solutions/card-game/part-12/frontend/src/client/collection.jsx
+++ advanced/exercise-solutions/card-game/part-12/frontend/src/client/collection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {withRouter} from "react-router-dom";
+import Card from "./card"
 
 export class Collection extends React.Component {
 
@@ -8,13 +9,40 @@ export class Collection extends React.Component {
 
         this.state = {
             userStats: null,
-            collection: null
+            collection: null,
+            openedPack: null
         }
     }
 
     componentDidMount() {
+        this.fetchCollection()
         this.fetchUserStats()
     }
+
+    fetchCollection = async () => {
+
+        const url = "/api/cards/collection_v1_000" ;
+
+        let response;
+
+        try {
+            response = await fetch(url, {method: "get"});
+        } catch (err) {
+            this.setState({errorMsg: "Failed to connect to server: " + err});
+            return;
+        }
+
+        if (response.status !== 200) {
+            this.setState({
+                errorMsg: "Failed connection to server. Status " + response.status,
+            });
+            return;
+        }
+
+        const payload = await response.json();
+        this.setState({collection: payload.data, errorMsg: null});
+    };
+
 
     fetchUserStats = async () => {
 
@@ -51,20 +79,81 @@ export class Collection extends React.Component {
             return;
         }
 
-        const stats = await response.json();
-        this.setState({userStats: stats.data, errorMsg: null});
+        const payload = await response.json();
+        this.setState({userStats: payload.data, errorMsg: null});
+
+        fetchUserStats()
     };
+
+
+    openPack = async () =>{
+        const userId = this.props.userId;
+        const url = "/api/user-collections/" + userId;
+
+        let response;
+
+        const data = {
+            command: "OPEN_PACK"
+        }
+
+        try {
+            response = await fetch(url, {
+                method: "patch",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(data)
+            });
+        } catch (err) {
+            this.setState({errorMsg: "Failed to connect to server: " + err});
+            return;
+        }
+
+        if (response.status === 401) {
+            this.props.updateLoggedInUser(null);
+            this.props.history.push("/");
+            return;
+        }
+
+        if (response.status === 404) {
+            this.setState({errorMsg: "Error: info not available for " + userId});
+            return;
+        }
+
+        if (response.status !== 200) {
+            this.setState({
+                errorMsg: "Failed connection to server. Status " + response.status,
+            });
+            return;
+        }
+
+        const payload = await response.json();
+        this.setState({openedPack: payload.data, errorMsg: null});
+    }
 
     render() {
 
         if(!this.state.userStats){
             return (<p>Loading user collection...</p>);
         }
+        if(!this.state.collection){
+            return (<p>Loading cards...</p>)
+        }
+
+        if(this.state.openedPack){
+            //TODO
+        }
 
         return (
             <div>
                 <p>Coins: {this.state.userStats.coins}</p>
                 <p>Packs: {this.state.userStats.cardPacks}</p>
+
+                <button>Open Pack</button>
+                {this.state.collection.cards.map(c => {
+                    const info = this.state.userStats.ownedCards.find(z => z.cardId===c.cardId)
+                    const quantity = info ? info.numberOfCopies : 0
+                     return  <Card {...c} quantity={quantity}/>
+                    }
+                    )}
             </div>
         );
     }

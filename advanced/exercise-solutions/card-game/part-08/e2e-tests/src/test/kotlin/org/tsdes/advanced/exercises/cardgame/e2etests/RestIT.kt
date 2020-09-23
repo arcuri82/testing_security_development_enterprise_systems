b579519ccc2d6d5d2fd329b322766bdc22cd5ae2
arcuri82 <arcuri82@gmail.com>
2020-09-23 20:34:36 +0200

    ex08

---
+++ advanced/exercise-solutions/card-game/part-08/e2e-tests/src/test/kotlin/org/tsdes/advanced/exercises/cardgame/e2etests/RestIT.kt
@@ -0,0 +1,122 @@
+package org.tsdes.advanced.exercises.cardgame.e2etests
+
+import io.restassured.RestAssured
+import io.restassured.RestAssured.given
+import io.restassured.http.ContentType
+import org.awaitility.Awaitility
+import org.hamcrest.CoreMatchers
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.Matchers.greaterThan
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.testcontainers.containers.DockerComposeContainer
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
+import java.io.File
+import java.time.Duration
+import java.util.concurrent.TimeUnit
+
+@Disabled
+@Testcontainers
+class RestIT {
+
+
+    companion object {
+
+        init {
+            RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
+            RestAssured.port = 80
+        }
+
+        class KDockerComposeContainer(id: String, path: File) : DockerComposeContainer<KDockerComposeContainer>(id, path)
+
+        @Container
+        @JvmField
+        val env = KDockerComposeContainer("card-game", File("../docker-compose.yml"))
+                .withExposedService("discovery", 8500,
+                        Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(240)))
+                .withLogConsumer("cards_0") { print("[CARD_0] " + it.utf8String) }
+                .withLogConsumer("cards_1") { print("[CARD_1] " + it.utf8String) }
+                .withLogConsumer("user-collections") { print("[USER_COLLECTIONS] " + it.utf8String) }
+                .withLogConsumer("scores") { print("[SCORES] " + it.utf8String) }
+                .withLocalCompose(true)
+
+
+        @BeforeAll
+        @JvmStatic
+        fun waitForServers() {
+
+            Awaitility.await().atMost(240, TimeUnit.SECONDS)
+                    .pollDelay(Duration.ofSeconds(20))
+                    .pollInterval(Duration.ofSeconds(10))
+                    .ignoreExceptions()
+                    .until {
+
+                        given().baseUri("http://${env.getServiceHost("discovery", 8500)}")
+                                .port(env.getServicePort("discovery", 8500))
+                                .get("/v1/agent/services")
+                                .then()
+                                .body("size()", equalTo(5))
+
+                        true
+                    }
+        }
+    }
+
+    @Test
+    fun testGetCollection() {
+        Awaitility.await().atMost(120, TimeUnit.SECONDS)
+                .pollInterval(Duration.ofSeconds(10))
+                .ignoreExceptions()
+                .until {
+                    given().get("/api/cards/collection_v1_000")
+                            .then()
+                            .statusCode(200)
+                            .body("data.cards.size", greaterThan(10))
+                    true
+                }
+    }
+
+    @Test
+    fun testGetScores() {
+        Awaitility.await().atMost(120, TimeUnit.SECONDS)
+                .pollInterval(Duration.ofSeconds(10))
+                .ignoreExceptions()
+                .until {
+                    given().accept(ContentType.JSON)
+                            .get("/api/scores")
+                            .then()
+                            .statusCode(200)
+                            .body("data.list.size()", greaterThan(0))
+                    true
+                }
+    }
+
+    @Test
+    fun testCreateUser() {
+        Awaitility.await().atMost(120, TimeUnit.SECONDS)
+                .pollInterval(Duration.ofSeconds(10))
+                .ignoreExceptions()
+                .until {
+
+                    val id = "foo_testCreateUser_" + System.currentTimeMillis()
+
+                    given().get("/api/user-collections/$id")
+                            .then()
+                            .statusCode(404)
+
+
+                    given().put("/api/user-collections/$id")
+                            .then()
+                            .statusCode(201)
+
+                    given().get("/api/user-collections/$id")
+                            .then()
+                            .statusCode(200)
+
+                    true
+                }
+    }
+}
--- doc/advanced/exercises/card-game/card-game-part-08.md
+++ doc/advanced/exercises/card-game/card-game-part-08.md
@@ -53,6 +53,7 @@ Recall that the names for the services you choose in this file are
 very important, as those will resolvable from the Docker-Compose's DNS.
 This means that, when in the `application.yml` files you need to define
 the URLs for Consul and Postgres instances, you will need to use those names.
+The APIs needing a database must use Postgres instead of H2.
 
 You need to open the port for the `api-gateway`, e.g.,
 ```
@@ -63,8 +64,39 @@ This must be the ONLY port open, and reachable from outside Docker-Compose,
 i.e., all incoming communications must go through the gateway.
 
 
-## E2E Tests
+## End-to-End Tests
 
+Create a `e2e-tests` module for End-to-End tests.
+Make sure that the `failsafe` plugin is configured/activated, and that
+your test'names end with `IT` and not `Test` (so that they will be executed
+by `failsafe` instead of `surefire`, after the Maven `package` phase).
+Also make sure to add a dependency to all the other modules, 
+to force Maven to build this `e2e-tests` module last.
+This is to enforce that, when running any of the `*IT.kt` tests, all the needed
+`*.jar` executables have been packaged.
+
+Besides `rest-assured`, to write E2E tests you will also need:
+
+* `org.testcontainers:testcontainers`
+* `org.awaitility:awaitility`
+
+Write a test for each of the REST APIs, in which they are accessed through
+the Gateway.
+You will need to use TestContainers to start the `docker-compose.yml`,
+and Awaitility to wait for everything being up and running before starting 
+the tests.
+This is particularly important to wait for Consul initialization, when all
+services have been registered (otherwise, the Gateway will not be able to route
+to the right running instances).  
+
+
+
+
+
+ 
+
+
+ 
 
 
 

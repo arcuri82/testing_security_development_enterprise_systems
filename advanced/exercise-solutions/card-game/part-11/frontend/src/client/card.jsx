cf73dd57c26cf8155de4fa5954c0fab09418c8cf
Sara Dalfalk <sara.dalfalk@novacare.no>
2020-11-04 19:53:52 +0100

    Logic for disabled card  and diffrent colord farmes

--- advanced/exercise-solutions/card-game/part-11/frontend/src/client/card.jsx
+++ advanced/exercise-solutions/card-game/part-11/frontend/src/client/card.jsx
@@ -4,14 +4,21 @@ import React from "react";
 const Card = (props) =>{
 
     let label = props.name;
+    
+    let imageClasses = "card-image";
+    
     if(props.quantity){
         label += " ("+props.quantity+")"
+    } else {
+        imageClasses += " disabled"
     }
 
+    let divClasses = "card " + props.rarity; 
+
     return(
-        <div className="card">
-            <h3>{label}</h3>
-            <img className="card-image" src={"/api/cards/imgs/"+props.imageId}/>
+        <div className={divClasses}>
+            <h3 className="card-label">{label}</h3>
+            <img className={imageClasses} src={"/api/cards/imgs/"+props.imageId}/>
         </div>
     );
 };

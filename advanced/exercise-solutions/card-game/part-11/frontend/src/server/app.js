ef4aa45b9fac283741be674d9fc2cf6b2e5001de
arcuri82 <arcuri82@gmail.com>
2020-11-04 21:02:04 +0100

    fixing ip address for dev mode

--- advanced/exercise-solutions/card-game/part-11/frontend/src/server/app.js
+++ advanced/exercise-solutions/card-game/part-11/frontend/src/server/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 if(process.env.DEV){
-    const proxiedServer = 'http://192.168.1.119:80'
+    const proxiedServer = 'http://128.0.0.1:80'
     app.use("/api/*", createProxyMiddleware({ target: proxiedServer, changeOrigin: true }))
 }
 

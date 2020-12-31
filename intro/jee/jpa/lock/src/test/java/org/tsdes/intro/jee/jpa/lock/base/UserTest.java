15414e36e6037a3cf6676ae1e709aaa793e83c69
arcuri82 <arcuri82@gmail.com>
2020-12-31 01:16:32 +0100

    simplified examples for Locks

--- intro/jee/jpa/lock/src/test/java/org/tsdes/intro/jee/jpa/lock/base/UserTest.java
+++ intro/jee/jpa/lock/src/test/java/org/tsdes/intro/jee/jpa/lock/base/UserTest.java
@@ -127,9 +127,9 @@ public class UserTest {
 
 
     @Test
-    public void testPessimisticRead() {
+    public void testReadWhilePessimisticLock() {
 
-        String name = "pessimisticRead";
+        String name = "read-pessimistic";
 
         long id = createUser(name);
 
@@ -137,7 +137,12 @@ public class UserTest {
         EntityTransaction tx = em.getTransaction();
 
         tx.begin();
-        User u1 = em.find(User.class, id, LockModeType.PESSIMISTIC_READ);
+        /*
+            WARNING: there are different kinds of PESSIMISTIC locks... but how they are
+            handled (if handled at all) does depend on the actual database (eg, H2 vs Postgres).
+            Even worse, if some modes are not supported, Hibernate will just silently ignore it...
+         */
+        User u1 = em.find(User.class, id, LockModeType.PESSIMISTIC_WRITE);
         assertNotNull(u1);
 
         TransactionExecutor executor = new TransactionExecutor(factory);
@@ -152,9 +157,9 @@ public class UserTest {
     }
 
     @Test
-    public void testPessimisticReadWithWrite() throws Exception {
+    public void testWriteWhilePessimisticLock() throws Exception {
 
-        String name = "pessimisticRead";
+        String name = "write-pessimistic";
         String other = "foo";
 
         long id = createUser(name);
@@ -163,7 +168,7 @@ public class UserTest {
         EntityTransaction tx = em.getTransaction();
 
         tx.begin();
-        User u1 = em.find(User.class, id, LockModeType.PESSIMISTIC_READ);
+        User u1 = em.find(User.class, id, LockModeType.PESSIMISTIC_WRITE);
         assertNotNull(u1);
 
         TransactionExecutor executor = new TransactionExecutor(factory);
@@ -192,165 +197,4 @@ public class UserTest {
         em.close();
     }
 
-
-    @Test
-    public void testPessimisticWriteWithPessimisticRead() throws Exception {
-
-        String name = "pessimisticWrite";
-
-        long id = createUser(name);
-
-        EntityManager em = factory.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-
-        tx.begin();
-        User u1 = em.find(User.class, id, LockModeType.PESSIMISTIC_WRITE);
-        assertNotNull(u1);
-
-        TransactionExecutor executor = new TransactionExecutor(factory);
-        Thread t = executor.asyncExe(s -> {
-            User user = s.find(User.class, id, LockModeType.PESSIMISTIC_READ);//PESSIMISTIC_WRITE would do as well
-            String readName = user.getName(); //just a read operation
-        });
-        //even if read operation, because lock is PESSIMISTIC_WRITE, still above is blocked
-
-        Thread.sleep(5_000); // simulate a long processing here
-
-        assertTrue(t.isAlive()); //the external transaction is still waiting for the lock
-
-        tx.commit();
-
-        //now that the lock is released, we can wait for the external transaction to finish
-
-        t.join();
-
-        assertFalse(t.isAlive());
-        em.close();
-    }
-
-    @Test
-    public void testPessimisticWriteWithRead() throws Exception {
-
-        String name = "pessimisticWrite";
-
-        long id = createUser(name);
-
-        EntityManager em = factory.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-
-        tx.begin();
-        User u1 = em.find(User.class, id, LockModeType.PESSIMISTIC_WRITE);
-        assertNotNull(u1);
-
-
-        TransactionExecutor executor = new TransactionExecutor(factory);
-        Thread t = executor.asyncExe(s -> {
-            User user = s.find(User.class, id); //no lock here
-            String readName = user.getName(); //just a read operation
-        });
-
-        /*
-            This can be confusing. if you google for LockModeType, you might read online
-            PESSIMISTIC_WRITE does prevent read operations in other transactions.
-            this does not really seem true, unless the other transactions do explicitly
-            try to use a lock.
-            Further note: pessimistic locking is relying on the locking mechanism
-            of the actual database, and that might lead to differences among different
-            databases
-         */
-
-        t.join(2_000);
-        assertFalse(t.isAlive());
-
-        tx.commit();
-        em.close();
-    }
-
-
-    @Test
-    public void testPessimisticReadWithUpdate() throws Exception {
-
-        String name = "pessimisticRead";
-        String other = "foo";
-
-        long id = createUser(name);
-
-        EntityManager em = factory.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-
-        assertEquals(name, getName(id));
-
-        tx.begin();
-        User u1 = em.find(User.class, id, LockModeType.PESSIMISTIC_READ);
-        assertNotNull(u1);
-        //read lock does not prevent us from updating the entity
-        u1.setName("foo");
-        tx.commit();
-
-        assertEquals(other, getName(id));
-
-        em.close();
-    }
-
-
-    @Test
-    public void testPessimisticReadWithPessimisticRead() throws Exception {
-
-        String name = "pessimisticRead";
-
-        long id = createUser(name);
-
-        EntityManager em = factory.createEntityManager();
-        EntityTransaction tx = em.getTransaction();
-
-        tx.begin();
-        User u1 = em.find(User.class, id, LockModeType.PESSIMISTIC_READ);
-        assertNotNull(u1);
-
-        TransactionExecutor executor = new TransactionExecutor(factory);
-        Thread t = executor.asyncExe(s -> {
-            User user = s.find(User.class, id, LockModeType.PESSIMISTIC_READ);
-            String readName = user.getName(); //just a read operation
-        });
-        /*
-            even if it just a read operation, because it tries to put a lock
-            on an already locked entity, it will block.
-
-            Again: if you read JEE documentation, this should not really happen,
-            as you should be able to get a read lock if no one else is having
-            a write one.
-
-            However, PESSIMISTIC_READ does depend on the actual database.
-            If the database does not support it, it would default to
-            PESSIMISTIC_WRITE
-         */
-
-        Thread.sleep(5_000); // simulate a long processing here
-
-        assertTrue(t.isAlive()); //the external transaction is still waiting for the lock
-
-        tx.commit();
-
-        //now that the lock is released, we can wait for the external transaction to finish
-
-        t.join();
-
-        assertFalse(t.isAlive());
-        em.close();
-    }
-
-    /*
-        In conclusion:
-
-        pessimistic locks are handled by the database, and not by the JEE container (eg JPA).
-        Documentation about them can be quite confusing.
-        To be on safe side, just use PESSIMISTIC_WRITE when you need to enforce locking and
-        conflicts are expected to be high, as PESSIMISTIC_READ might not be supported (
-        and so default to PESSIMISTIC_WRITE that should always be available).
-        Do write test cases to check if your understanding of locks is indeed correct.
-
-        At any rate, the difference when choosing between PESSIMISTIC_READ and PESSIMISTIC_WRITE should
-        be mainly for performance. But performance should not be evaluated on an embedded database,
-        but rather on a real, production one.
-     */
 }
